925316d7c4e1c6df7ac4c2d2e12b9834425087a6
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-22 14:46:22 -0300

    feat: I created the addAgent function

--- lib/app/data/data_source/agent_data_source.dart
+++ lib/app/data/data_source/agent_data_source.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:control_concierge_agents/app/data/enums/bond_type_enum.dart';
 import 'package:control_concierge_agents/app/data/models/agent_model.dart';
 import 'package:dartz/dartz.dart';
 import '../../core/helpers/errors/errors.dart';
 
 class AgentDataSource {
+  final _firestore = FirebaseFirestore.instance;
+
   Future<Either<CommonError, List<AgentModel>>> getAgents() async {
     try {
       return Right(
@@ -10,21 +14,33 @@ class AgentDataSource {
           AgentModel(
             id: 01,
             name: 'Josinaldo',
-            employmentRelationship: 'Contrato',
+            bondType: BondTypeEnum.effective,
             unit: 'Esc Dey',
-            vacation: 'Não possui',
+            vacationMonth: 'Não possui',
             phone: '99 99999-9999',
           ),
           AgentModel(
             id: 02,
             name: 'Samuel',
-            employmentRelationship: 'Efetivo',
+            bondType: BondTypeEnum.contract,
             unit: 'Esc Osvaldo Rodrigues',
-            vacation: 'Janeiro',
+            vacationMonth: 'Janeiro',
             phone: '99 99999-9999',
           ),
         ],
       );
+    } on Exception catch (e) {
+      return Left(GenerateError.fromException(e));
+    }
+  }
+
+  Future<Either<CommonError, bool>> addAgent(AgentModel agent) async {
+    try {
+      await _firestore
+          .collection('concierge-agents')
+          .doc(agent.id.toString())
+          .set(agent.toMap());
+      return const Right(true);
     } on Exception catch (e) {
       return Left(GenerateError.fromException(e));
     }
