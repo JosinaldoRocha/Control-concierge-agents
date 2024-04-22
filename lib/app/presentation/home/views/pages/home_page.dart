5bb16746f73cdb71e760f3a9668d453c970d7d39
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-22 08:56:56 -0300

    feat: Agent state implementation

--- lib/app/presentation/home/views/pages/home_page.dart
+++ lib/app/presentation/home/views/pages/home_page.dart
@@ -1,32 +1,49 @@
-import 'package:control_concierge_agents/app/core/style/app_colors.dart';
+import 'package:control_concierge_agents/app/presentation/home/provider/agent_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  ConsumerState<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  final agents = ['Ronaldo', 'Luiz', 'Sandro', 'Rafael'];
+class _HomePageState extends ConsumerState<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() => ref.read(agentListStateProvider.notifier).load());
+  }
 
   @override
   Widget build(BuildContext context) {
+    final state = ref.watch(agentListStateProvider);
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Agentes de portaria SEMED'),
+      appBar: AppBar(
+        title: const Text('Agentes de portaria SEMED'),
+      ),
+      body: state.maybeWhen(
+        loadInProgress: () => const Center(
+          child: CircularProgressIndicator(),
         ),
-        body: ListView.separated(
-          padding: const EdgeInsets.all(16),
-          itemBuilder: (context, index) => ElevatedButton(
+        loadSuccess: (data) {
+          return ListView.separated(
+            padding: const EdgeInsets.all(16),
+            itemBuilder: (context, index) => ElevatedButton(
               onPressed: () {},
-              child: Text(
-                agents[index],
-                style: const TextStyle(color: AppColor.white),
-              )),
-          separatorBuilder: (context, index) => const SizedBox(height: 20),
-          itemCount: agents.length,
-        ));
+              child: Text(data[index].name),
+            ),
+            separatorBuilder: (context, index) => const SizedBox(height: 0),
+            itemCount: data.length,
+          );
+        },
+        loadFailure: (failure) => const SizedBox(
+          child: Text('Ocorreu um erro'),
+        ),
+        orElse: () => Container(),
+      ),
+    );
   }
 }
