e82acf3095e7cb81b32ed2751c70b7ba8ff60335
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-22 08:54:16 -0300

    feat: I created the AgentModel class

---
+++ lib/app/data/models/agent_model.dart
@@ -0,0 +1,16 @@
+class AgentModel {
+  int id;
+  String name;
+  String employmentRelationship;
+  String unit;
+  String vacation;
+  String phone;
+  AgentModel({
+    required this.id,
+    required this.name,
+    required this.employmentRelationship,
+    required this.unit,
+    required this.vacation,
+    required this.phone,
+  });
+}
