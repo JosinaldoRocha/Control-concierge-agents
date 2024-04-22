84700e78a6061875fc556a461529234074ca2920
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-22 14:47:00 -0300

    feat: I created the InputWidget widget

---
+++ lib/app/widgets/input_widget.dart
@@ -0,0 +1,133 @@
+import 'package:control_concierge_agents/app/core/style/app_colors.dart';
+import 'package:control_concierge_agents/app/core/style/app_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class InputWidget extends StatelessWidget {
+  final TextEditingController controller;
+  final Widget? prefix;
+  final Widget? sufix;
+  final TextInputType keyboardType;
+  final bool isEnabled;
+
+  final String? hintText;
+  final bool? obscureText;
+
+  final String? Function(String?)? validator;
+  final ValueChanged<String>? onChanged;
+  final Iterable<String>? autofillHints;
+  final bool readOnly;
+  final TextInputAction? action;
+  final Function(String)? onSubmitted;
+  final List<TextInputFormatter>? inputFormatters;
+  final int? lines;
+  final int? minLines;
+
+  final AutovalidateMode? autoValidate;
+  final FocusNode? focus;
+  final bool autoFocus;
+
+  /// FormInput
+  ///
+  /// [controller] form controller
+  ///
+  /// [prefix] is a prefix of input will be rendered on left side of input
+  ///
+  /// [type] type of text input
+  ///
+  /// [isEnable] is a bool that will be check if formInput is active
+  ///
+  /// [validator] function that validate input
+  ///
+  /// [onChanged] onChanged of formInput
+  InputWidget({
+    required this.controller,
+    this.isEnabled = true,
+    this.prefix,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+    this.onChanged,
+    this.hintText,
+    this.sufix,
+    this.autofillHints,
+    this.obscureText,
+    this.readOnly = false,
+    this.inputFormatters,
+    this.action,
+    this.onSubmitted,
+    this.lines,
+    this.autoValidate,
+    this.focus,
+    this.autoFocus = false,
+    this.minLines,
+    super.key,
+  });
+
+  final _focus = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Focus(
+      child: Builder(
+        builder: (context) {
+          final FocusNode focusNode = Focus.of(context);
+          final bool hasFocus = focusNode.hasFocus;
+          return TextFormField(
+            focusNode: focus ?? _focus,
+            validator: validator,
+            minLines: minLines,
+            autofocus: autoFocus,
+            autovalidateMode: autoValidate,
+            onChanged: onChanged,
+            enabled: isEnabled,
+            autofillHints: autofillHints,
+            controller: controller,
+            textInputAction: action,
+            textAlignVertical: TextAlignVertical.center,
+            onFieldSubmitted: onSubmitted,
+            readOnly: readOnly,
+            cursorColor: AppColor.primary,
+            cursorHeight: 22,
+            inputFormatters: inputFormatters,
+            keyboardType: keyboardType,
+            obscureText: obscureText ?? false,
+            maxLines: lines,
+            style: AppText.text().bodyMedium,
+            textAlign: TextAlign.left,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 17.0,
+              ),
+              errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: const BorderSide(color: AppColor.error),
+              ),
+              errorStyle: const TextStyle(color: AppColor.error),
+              filled: true,
+              fillColor: hasFocus ? AppColor.lightBlue : AppColor.lightGrey,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: const BorderSide(color: AppColor.mediumBlue),
+              ),
+              focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: const BorderSide(color: AppColor.error),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide.none,
+              ),
+              prefixIcon: prefix,
+              hintStyle: AppText.text()
+                  .bodySmall
+                  ?.copyWith(color: AppColor.mediumBlue),
+              hintText: hintText,
+              suffixIcon: sufix,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
