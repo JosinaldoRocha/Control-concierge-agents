9e2d78b33d70182618b0db8dc45f5bce789df5e0
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-22 16:41:38 -0300

    feat: Form adjustment for agent registration

--- lib/app/presentation/agent/views/pages/add/add_agent_page.dart
+++ lib/app/presentation/agent/views/pages/add/add_agent_page.dart
@@ -1,7 +1,10 @@
+import 'package:control_concierge_agents/app/core/constants/constants.dart';
 import 'package:control_concierge_agents/app/data/enums/bond_type_enum.dart';
 import 'package:control_concierge_agents/app/presentation/agent/widgets/select_vacation_month_widget.dart';
+import 'package:control_concierge_agents/app/widgets/button/button_widget.dart';
 import 'package:control_concierge_agents/app/widgets/dropdown/dropdown_widget.dart';
-import 'package:control_concierge_agents/app/widgets/input_widget.dart';
+import 'package:control_concierge_agents/app/widgets/input/input_validators.dart';
+import 'package:control_concierge_agents/app/widgets/input/input_widget.dart';
 import 'package:control_concierge_agents/app/widgets/spacing/vertical_space_widget.dart';
 import 'package:dropdown_textfield/dropdown_textfield.dart';
 import 'package:flutter/material.dart';
@@ -15,26 +18,47 @@ class AddAgentPage extends StatefulWidget {
 
 class _AddAgentPageState extends State<AddAgentPage> {
   final nameController = TextEditingController();
-  final employmentRelationshipController = SingleValueDropDownController();
-  final unitController = TextEditingController();
+  final bondTypeController = SingleValueDropDownController();
+  final unitController = SingleValueDropDownController();
   final phoneNumberController = TextEditingController();
-  DateTime? vacationMonth;
+  final vacacionMonthContoller = SingleValueDropDownController();
+  final observationsController = TextEditingController();
+  final forKey = GlobalKey<FormState>();
 
-  Future<void> selectMonth() async {
+  DateTime? startVacation;
+  DateTime? endVacation;
+
+  Future<void> selectStartVacation() async {
     final DateTime? picked = await showDatePicker(
       context: context,
-      initialDate: vacationMonth,
+      initialDate: startVacation,
       firstDate: DateTime(DateTime.now().year, 1),
       lastDate: DateTime(DateTime.now().year, 12),
     );
-    //TODO ajustar seletor de data para mostrar apenas o mês
 
-    if (picked != null && picked != vacationMonth) {
+    if (picked != null && picked != startVacation) {
       setState(() {
-        vacationMonth = picked;
+        startVacation = picked;
       });
     }
   }
+
+  Future<void> selectEndVacation() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: endVacation,
+      firstDate: DateTime(DateTime.now().year, 1),
+      lastDate: DateTime(DateTime.now().year + 1, 1),
+    );
+
+    if (picked != null && picked != endVacation) {
+      setState(() {
+        endVacation = picked;
+      });
+    }
+  }
+
+  var teste = BondTypeEnum.effective;
 
   @override
   Widget build(BuildContext context) {
@@ -42,40 +66,87 @@ class _AddAgentPageState extends State<AddAgentPage> {
       appBar: AppBar(
         title: const Text(''),
       ),
-      body: ListView(
-        shrinkWrap: true,
+      body: Padding(
         padding: const EdgeInsets.all(16),
-        children: [
-          InputWidget(
-            controller: nameController,
-            hintText: 'Nome',
-          ),
-          const SpaceVertical.x4(),
-          DropDownWidget(
-            controller: employmentRelationshipController,
-            list: [
-              DropDownValueModel(name: BondTypeEnum.effective.text, value: 1),
-              DropDownValueModel(name: BondTypeEnum.contract.text, value: 2),
+        child: Form(
+          key: forKey,
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.only(bottom: 16),
+                  children: [
+                    InputWidget(
+                      controller: nameController,
+                      hintText: 'Nome',
+                      validator: InputValidators.fullName,
+                    ),
+                    const SpaceVertical.x4(),
+                    DropDownWidget(
+                      controller: bondTypeController,
+                      list: bondTypeList,
+                      hintText: 'Vínculo empregatício',
+                      onChanged: (p0) {
+                        setState(() {
+                          teste = p0.value;
+                        });
+                      },
+                    ),
+                    const SpaceVertical.x4(),
+                    DropDownWidget(
+                      controller: unitController,
+                      //TODO: tornar lista rolável
+                      list: unitList,
+                      hintText: 'Lotação',
+                    ),
+                    if (teste == BondTypeEnum.effective)
+                      Column(
+                        children: [
+                          const SpaceVertical.x4(),
+                          DropDownWidget(
+                            controller: vacacionMonthContoller,
+                            list: monthList,
+                            hintText: 'Mês de férias',
+                          ),
+                          const SpaceVertical.x4(),
+                          SelectVacationMonthWidget(
+                            hintText: 'Início das férias',
+                            birthDate: startVacation,
+                            onTap: selectStartVacation,
+                          ),
+                          const SpaceVertical.x4(),
+                          SelectVacationMonthWidget(
+                            hintText: 'Término das férias',
+                            birthDate: endVacation,
+                            onTap: selectEndVacation,
+                          ),
+                        ],
+                      ),
+                    const SpaceVertical.x4(),
+                    InputWidget(
+                      controller: phoneNumberController,
+                      hintText: 'Celular',
+                      validator: InputValidators.phone,
+                      keyboardType: TextInputType.phone,
+                    ),
+                    const SpaceVertical.x4(),
+                    InputWidget(
+                      controller: observationsController,
+                      hintText: 'Observações',
+                    ),
+                  ],
+                ),
+              ),
+              ButtonWidget(
+                title: 'Salvar agente',
+                onTap: () {
+                  if (forKey.currentState!.validate()) {}
+                },
+              ),
             ],
-            hintText: 'Vínculo empregatício',
           ),
-          const SpaceVertical.x4(),
-          InputWidget(
-            controller: unitController,
-            hintText: 'Lotação',
-          ),
-          const SpaceVertical.x4(),
-          InputWidget(
-            controller: phoneNumberController,
-            hintText: 'Celular',
-          ),
-          const SpaceVertical.x4(),
-          SelectVacationMonthWidget(
-            hintText: 'Mês das férias',
-            birthDate: vacationMonth,
-            onTap: selectMonth,
-          ),
-        ],
+        ),
       ),
     );
   }
