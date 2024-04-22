d53c5558eafdec4d107409ad99c52f66256a1378
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-22 08:57:44 -0300

    feat: Added the ProviderScope widget

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:control_concierge_agents/app/core/style/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/presentation/home/views/pages/home_page.dart';
 
 class AppWidget extends StatelessWidget {
@@ -7,11 +8,13 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Agentes de portaria SEMED',
-      theme: AppTheme.getTheme(),
-      home: const HomePage(),
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Agentes de portaria SEMED',
+        theme: AppTheme.getTheme(),
+        home: const HomePage(),
+      ),
     );
   }
 }
