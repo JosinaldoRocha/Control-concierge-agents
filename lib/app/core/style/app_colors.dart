76aecea8ea06219f41a904e460be148396bb327f
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-22 14:44:17 -0300

    feat: I created the error, lightGrey, lightBlue and mediumBlue parameters

--- lib/app/core/style/app_colors.dart
+++ lib/app/core/style/app_colors.dart
@@ -2,8 +2,11 @@ import 'dart:ui';
 
 class AppColor {
   static const bgColor = Color(0xFFFAFAFA);
-//  static const primary = Color(0xFFFF4D6F);
   static const primary = Color(0xFF242D72);
   static const secondary = Color(0xFFA4C754);
   static const white = Color(0xFFFFFFFF);
+  static const error = Color(0xFFFF7043);
+  static const lightGrey = Color(0xFFEAEAEA);
+  static const lightBlue = Color.fromARGB(146, 237, 244, 247);
+  static const mediumBlue = Color.fromARGB(255, 79, 89, 162);
 }
