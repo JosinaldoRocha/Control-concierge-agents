f497ba9de72d38927672080ba970f857ac8d666a
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-23 11:45:19 -0300

    feat: I created the AddAgentStateNotifier stata notifier

---
+++ lib/app/presentation/agent/states/add_agent_state_notifier.dart
@@ -0,0 +1,23 @@
+import 'package:control_concierge_agents/app/data/data_source/agent_data_source.dart';
+import 'package:control_concierge_agents/app/data/models/agent_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/helpers/helpers.dart';
+
+typedef AddAgentState = CommonState<CommonError, bool>;
+
+class AddAgentStateNotifier extends StateNotifier<AddAgentState> {
+  AddAgentStateNotifier({required this.dataSource})
+      : super(const AddAgentState.initial());
+
+  final AgentDataSource dataSource;
+
+  Future<void> add(AgentModel agent) async {
+    state = const AddAgentState.loadInProgress();
+
+    final result = await dataSource.addAgent(agent);
+    result.fold(
+      (l) => state = AddAgentState.loadFailure(l),
+      (r) => state = AddAgentState.loadSuccess(r),
+    );
+  }
+}
