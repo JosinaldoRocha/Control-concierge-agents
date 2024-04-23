c8d951ab5d1cd35cca5981061344e5c878659091
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-23 16:00:33 -0300

    feat: I created the monthEnum enum

---
+++ lib/app/data/enums/month_enum.dart
@@ -0,0 +1,43 @@
+enum MonthEnum {
+  january,
+  february,
+  march,
+  april,
+  may,
+  june,
+  july,
+  august,
+  september,
+  october,
+  november,
+  december;
+
+  String get text {
+    switch (this) {
+      case MonthEnum.january:
+        return 'Janeiro';
+      case MonthEnum.february:
+        return 'Fevereiro';
+      case MonthEnum.march:
+        return 'Março';
+      case MonthEnum.april:
+        return 'Abril';
+      case MonthEnum.may:
+        return 'Maio';
+      case MonthEnum.june:
+        return 'Junho';
+      case MonthEnum.july:
+        return 'Julho';
+      case MonthEnum.august:
+        return 'Augusto';
+      case MonthEnum.september:
+        return 'Setembro';
+      case MonthEnum.october:
+        return 'Outubro';
+      case MonthEnum.november:
+        return 'Novembro';
+      case MonthEnum.december:
+        return 'Dezembro';
+    }
+  }
+}
