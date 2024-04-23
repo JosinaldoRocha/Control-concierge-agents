18134695556ae257dead8d56d369715f2feb577e
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-23 14:59:46 -0300

    fix: I changed the logic of the getAgents function

--- lib/app/data/data_source/agent_data_source.dart
+++ lib/app/data/data_source/agent_data_source.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:control_concierge_agents/app/data/enums/bond_type_enum.dart';
 import 'package:control_concierge_agents/app/data/models/agent_model.dart';
 import 'package:dartz/dartz.dart';
 import '../../core/helpers/errors/errors.dart';
@@ -9,26 +8,18 @@ class AgentDataSource {
 
   Future<Either<CommonError, List<AgentModel>>> getAgents() async {
     try {
-      return Right(
-        [
-          AgentModel(
-            id: '01',
-            name: 'Josinaldo',
-            bondType: BondTypeEnum.effective,
-            unit: 'Esc Dey',
-            vacationMonth: 'Não possui',
-            phone: '99 99999-9999',
-          ),
-          AgentModel(
-            id: '02',
-            name: 'Samuel',
-            bondType: BondTypeEnum.contract,
-            unit: 'Esc Osvaldo Rodrigues',
-            vacationMonth: 'Janeiro',
-            phone: '99 99999-9999',
-          ),
-        ],
-      );
+      final getDocuments =
+          await _firestore.collection('concierge-agents').get();
+      final documents = getDocuments.docs;
+      List<AgentModel> agents = [];
+
+      for (var docs in documents) {
+        final item = AgentModel.fromSnapShot(docs);
+        agents.add(item);
+      }
+      agents.sort((a, b) => a.name.compareTo(b.name));
+
+      return Right(agents);
     } on Exception catch (e) {
       return Left(GenerateError.fromException(e));
     }
@@ -38,7 +29,7 @@ class AgentDataSource {
     try {
       await _firestore
           .collection('concierge-agents')
-          .doc(agent.id.toString())
+          .doc(agent.id)
           .set(agent.toMap());
       return const Right(true);
     } on Exception catch (e) {
