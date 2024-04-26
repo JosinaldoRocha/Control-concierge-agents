e2bda9314dac62c4296fd87db34dd1a8997a8417
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-26 18:36:22 -0300

    feat: I created the deleteAgentStateProvider provider

--- lib/app/presentation/agent/provider/agent_provider.dart
+++ lib/app/presentation/agent/provider/agent_provider.dart
@@ -1,10 +1,18 @@
 import 'package:control_concierge_agents/app/data/providers/data_provider.dart';
+import 'package:control_concierge_agents/app/presentation/agent/states/delete_agent_state_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../states/add_agent_state_notifier.dart';
 
 final addAgentStateProvider =
     StateNotifierProvider<AddAgentStateNotifier, AddAgentState>(
   (ref) => AddAgentStateNotifier(
+    dataSource: ref.read(agentDataSourceProvider),
+  ),
+);
+
+final deleteAgentStateProvider =
+    StateNotifierProvider<DeleteAgentStateNotifier, DeleteAgentState>(
+  (ref) => DeleteAgentStateNotifier(
     dataSource: ref.read(agentDataSourceProvider),
   ),
 );
