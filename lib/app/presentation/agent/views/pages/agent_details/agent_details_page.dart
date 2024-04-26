3307975b3f1ebed818597127baa39f63e1d3d481
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-26 15:51:58 -0300

    feat: I created the _buildButton function and added the Row and SpaceHorizontal widgets

--- lib/app/presentation/agent/views/pages/agent_details/agent_details_page.dart
+++ lib/app/presentation/agent/views/pages/agent_details/agent_details_page.dart
@@ -1,4 +1,6 @@
+import 'package:control_concierge_agents/app/core/style/app_colors.dart';
 import 'package:control_concierge_agents/app/presentation/agent/widgets/agent_details_widget.dart';
+import 'package:control_concierge_agents/app/widgets/spacing/space_horizontal_widget.dart';
 import 'package:control_concierge_agents/app/widgets/spacing/vertical_space_widget.dart';
 import 'package:flutter/material.dart';
 import '../../../../../data/models/agent_model.dart';
@@ -31,6 +33,44 @@ class AgentDetailsPage extends StatelessWidget {
           const SpaceVertical.x4(),
           AgentDetailsWidget(agent: agent),
           const SpaceVertical.x5(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              _buildButton(true),
+              const SpaceHorizontal.x4(),
+              _buildButton(false),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  ElevatedButton _buildButton(bool isEditButton) {
+    final getColor = isEditButton ? AppColor.primary : AppColor.primaryRed;
+
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          elevation: 0,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          backgroundColor: AppColor.white,
+          side: BorderSide(
+            color: getColor,
+          )),
+      onPressed: () {},
+      child: Row(
+        children: [
+          Text(
+            isEditButton ? 'Editar' : 'Excluir',
+            style: TextStyle(
+              color: getColor,
+            ),
+          ),
+          const SpaceHorizontal.x2(),
+          Icon(
+            isEditButton ? Icons.edit : Icons.delete,
+            color: getColor,
+          ),
         ],
       ),
     );
