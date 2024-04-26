98c04c99f88eeacf7b34ad35fc354443ffd62259
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-26 18:38:43 -0300

    feat: I created the DeleteAgentStateNotifier state notifier

---
+++ lib/app/presentation/agent/states/delete_agent_state_notifier.dart
@@ -0,0 +1,22 @@
+import 'package:control_concierge_agents/app/data/data_source/agent_data_source.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/helpers/helpers.dart';
+
+typedef DeleteAgentState = CommonState<CommonError, bool>;
+
+class DeleteAgentStateNotifier extends StateNotifier<DeleteAgentState> {
+  DeleteAgentStateNotifier({required this.dataSource})
+      : super(const DeleteAgentState.initial());
+
+  final AgentDataSource dataSource;
+
+  Future<void> delete(String agentId) async {
+    state = const DeleteAgentState.loadInProgress();
+
+    final result = await dataSource.deleteAgent(agentId);
+    result.fold(
+      (l) => state = DeleteAgentState.loadFailure(l),
+      (r) => state = DeleteAgentState.loadSuccess(r),
+    );
+  }
+}
