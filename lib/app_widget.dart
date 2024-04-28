2da4b7708d225725ff3011d0bee176112e44ae86
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-28 13:12:27 -0300

    feat: I called the locale, supportedLocales and localizationsDelegates properties

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -2,6 +2,7 @@ import 'package:control_concierge_agents/app/app_routes.dart';
 import 'package:control_concierge_agents/app/core/style/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -15,6 +16,13 @@ class AppWidget extends StatelessWidget {
         theme: AppTheme.getTheme(),
         initialRoute: AppRoutes().initialRoute,
         routes: AppRoutes().allAppRoutes,
+        locale: const Locale('pt', 'BR'),
+        supportedLocales: const [Locale('pt', 'BR')],
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
       ),
     );
   }
