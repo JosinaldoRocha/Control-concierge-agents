adff365dfed311b239acdfca1f6e9104095cdbf5
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-29 15:37:32 -0300

    feat: I created the editAgentStateProvider provider

--- lib/app/presentation/agent/provider/agent_provider.dart
+++ lib/app/presentation/agent/provider/agent_provider.dart
@@ -2,6 +2,7 @@ import 'package:control_concierge_agents/app/data/providers/data_provider.dart';
 import 'package:control_concierge_agents/app/presentation/agent/states/delete_agent_state_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../states/add_agent_state_notifier.dart';
+import '../states/edit/edit_agent_state_notifier.dart';
 
 final addAgentStateProvider =
     StateNotifierProvider<AddAgentStateNotifier, AddAgentState>(
@@ -13,6 +14,13 @@ final addAgentStateProvider =
 final deleteAgentStateProvider =
     StateNotifierProvider<DeleteAgentStateNotifier, DeleteAgentState>(
   (ref) => DeleteAgentStateNotifier(
+    dataSource: ref.read(agentDataSourceProvider),
+  ),
+);
+
+final editAgentStateProvider =
+    StateNotifierProvider<EditAgentStateNotifier, EditAgentState>(
+  (ref) => EditAgentStateNotifier(
     dataSource: ref.read(agentDataSourceProvider),
   ),
 );
