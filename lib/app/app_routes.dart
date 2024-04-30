2a84770765fd22ce77695bc3f8082656015edff4
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-30 16:33:17 -0300

    feat: I created the '/' route to SplashPage screen and i changed the value of the initialRoute parameter

--- lib/app/app_routes.dart
+++ lib/app/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:control_concierge_agents/app/presentation/agent/routes/agent_routes.dart';
+import 'package:control_concierge_agents/app/presentation/splash/views/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'core/navigator/navigator.dart';
 import 'presentation/home/views/pages/home_page.dart';
@@ -10,10 +11,11 @@ class AppRoutes extends IAppRoutes {
       ];
 
   @override
-  String get initialRoute => '/home';
+  String get initialRoute => '/';
 
   @override
   Map<String, Widget Function(BuildContext p1)> get routes => {
+        '/': (_) => const SplashPage(),
         '/home': (_) => const HomePage(),
       };
 }
