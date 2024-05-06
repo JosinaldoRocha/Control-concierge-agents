54a1c319954e05e6c5d26660c01d132024aa6d3b
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-05-06 16:51:14 -0300

    feat: I created the filter and containsFilter parameters, i created the filterToString function, i called the minimumSize property and added the Column, Divider and Text widgets

--- lib/app/presentation/home/views/widgets/agent_item_widget.dart
+++ lib/app/presentation/home/views/widgets/agent_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:control_concierge_agents/app/core/style/app_colors.dart';
+import 'package:control_concierge_agents/app/data/enums/filter_type_enum.dart';
 import 'package:control_concierge_agents/app/data/models/agent_model.dart';
 import 'package:control_concierge_agents/app/widgets/spacing/space_horizontal_widget.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +8,15 @@ class AgentItemWidget extends StatelessWidget {
   const AgentItemWidget({
     super.key,
     required this.agent,
+    this.filter,
   });
   final AgentModel agent;
+  final FilterType? filter;
 
   @override
   Widget build(BuildContext context) {
+    bool containsFilter = filter != null && filter != FilterType.name;
+
     String getInitials(String name) {
       List<String> nameParts = name.split(' ');
       String initials = '';
@@ -23,22 +28,30 @@ class AgentItemWidget extends StatelessWidget {
           initials += part[0];
           count++;
         }
-
         if (count == 2) break;
       }
-
       return initials;
     }
 
-    String agentInitials = getInitials(agent.name);
+    String filterToString() {
+      switch (filter) {
+        case FilterType.bondType:
+          return agent.bondType.text;
+        case FilterType.unit:
+          return agent.unit;
+        default:
+          return agent.workShift;
+      }
+    }
 
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         elevation: 0,
         padding: const EdgeInsets.symmetric(
           horizontal: 10,
-          vertical: 8,
+          vertical: 6,
         ),
+        minimumSize: Size(200, 48),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
           side: const BorderSide(color: AppColor.primary),
@@ -52,24 +65,43 @@ class AgentItemWidget extends StatelessWidget {
           arguments: agent,
         );
       },
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            child: Text(
-              agent.name,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(color: AppColor.primary),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Text(
+                  agent.name,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(color: AppColor.primary),
+                ),
+              ),
+              const SpaceHorizontal.x2(),
+              Text(
+                getInitials(agent.name),
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: AppColor.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          if (containsFilter)
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Divider(
+                  color: AppColor.lightGrey,
+                ),
+                Text(
+                  filterToString(),
+                  style: TextStyle(color: AppColor.error),
+                ),
+              ],
             ),
-          ),
-          const SpaceHorizontal.x2(),
-          Text(
-            agentInitials,
-            style: const TextStyle(
-                fontSize: 18,
-                color: AppColor.primary,
-                fontWeight: FontWeight.bold),
-          ),
         ],
       ),
     );
