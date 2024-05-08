778b6334cc6bd380ff049e0fe3f8ff7a576b9761
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-05-08 10:30:16 -0300

    feat: I created the SelectFilterOptionWidget widget

---
+++ lib/app/presentation/home/views/widgets/select_filter_option_widget.dart
@@ -0,0 +1,50 @@
+import 'package:control_concierge_agents/app/data/enums/filter_type_enum.dart';
+import 'package:dropdown_textfield/dropdown_textfield.dart';
+import 'package:flutter/material.dart';
+import '../../../../core/constants/constants.dart';
+import 'filter_dropdown_widget.dart';
+
+class SelectFilterOptionWidget extends StatelessWidget {
+  const SelectFilterOptionWidget({
+    super.key,
+    required this.filterType,
+    required this.bondTypeController,
+    required this.unitController,
+    required this.workShiftController,
+    required this.onChanged,
+  });
+  final FilterType? filterType;
+  final SingleValueDropDownController bondTypeController;
+  final SingleValueDropDownController unitController;
+  final SingleValueDropDownController workShiftController;
+  final Function(dynamic) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    if (filterType! == FilterType.bondType) {
+      return FilterDropDownWidget(
+        controller: bondTypeController,
+        list: bondTypeList,
+        hintText: 'Selecione o vínculo',
+        onChanged: onChanged,
+      );
+    } else if (filterType! == FilterType.unit) {
+      return FilterDropDownWidget(
+        controller: unitController,
+        list: unitList,
+        hintText: 'Selecione a lotação',
+        listPadding: ListPadding(top: 5, bottom: 5),
+        onChanged: onChanged,
+      );
+    } else if (filterType! == FilterType.workShift) {
+      return FilterDropDownWidget(
+        controller: workShiftController,
+        list: workShiftList,
+        hintText: 'Selecione o turno',
+        onChanged: onChanged,
+      );
+    } else {
+      return Container();
+    }
+  }
+}
