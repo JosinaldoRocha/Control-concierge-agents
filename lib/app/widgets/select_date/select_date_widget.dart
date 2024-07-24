97f31a20571b43d79513e94474b15e4eb0e83aab
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-07-24 08:47:51 -0300

    feat: i created the validator parameter, i called the crossAxisAlignment and border property, i changed the value of the style property and i called the Padding and Text widgets

--- lib/app/widgets/select_date/select_date_widget.dart
+++ lib/app/widgets/select_date/select_date_widget.dart
@@ -6,16 +6,19 @@ import 'package:intl/intl.dart';
 
 class SelectDateWidget extends StatefulWidget {
   const SelectDateWidget({
-    super.key,
+    Key? key,
     required this.date,
     required this.onTap,
     required this.hintText,
     required this.onClean,
-  });
+    this.validator, // Adicionando o validador aqui
+  }) : super(key: key);
+
   final DateTime? date;
   final Function() onTap;
   final String hintText;
   final Function() onClean;
+  final String? Function(DateTime?)? validator; // Função de validação de data
 
   @override
   State<SelectDateWidget> createState() => _SelectDateWidgetState();
@@ -25,6 +28,7 @@ class _SelectDateWidgetState extends State<SelectDateWidget> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         InkWell(
           onTap: widget.onTap,
@@ -34,6 +38,9 @@ class _SelectDateWidgetState extends State<SelectDateWidget> {
             decoration: BoxDecoration(
               color: AppColor.lightGrey,
               borderRadius: BorderRadius.circular(12),
+              border: widget.validator != null && widget.date == null
+                  ? Border.all(color: AppColor.error)
+                  : null,
             ),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
@@ -44,11 +51,10 @@ class _SelectDateWidgetState extends State<SelectDateWidget> {
                     widget.date == null
                         ? widget.hintText
                         : DateFormat('dd/MM/yyyy').format(widget.date!),
-                    style: AppText.text().bodyMedium!.copyWith(
+                    style: AppText.text().titleMedium!.copyWith(
                           color: widget.date == null
                               ? AppColor.mediumBlue
                               : Colors.black,
-                          fontSize: 16,
                         ),
                   ),
                   widget.date != null
@@ -68,6 +74,17 @@ class _SelectDateWidgetState extends State<SelectDateWidget> {
             ),
           ),
         ),
+        if (widget.validator != null && widget.date == null)
+          Padding(
+            padding: EdgeInsets.only(top: 8, left: 20),
+            child: Text(
+              widget.validator!(widget.date).toString(),
+              style: AppText.text().bodySmall!.copyWith(
+                    color: AppColor.error,
+                    fontSize: 12,
+                  ),
+            ),
+          ),
       ],
     );
   }
