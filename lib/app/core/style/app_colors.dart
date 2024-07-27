18099335d6dc085b510aca62bc94249c9c99d0d8
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-07-27 18:12:34 -0300

    feat: i created the lightGreen, primaryGrey and mediunGreen parameters

--- lib/app/core/style/app_colors.dart
+++ lib/app/core/style/app_colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class AppColor {
-  static const bgColor = Color(0xFFFAFAFA);
+  static const bgColor = Color(0xFFF1F0F0);
   static const primary = Color(0xFF242D72);
   static const secondary = Color(0xFFA4C754);
   static const white = Color(0xFFFFFFFF);
@@ -12,4 +12,7 @@ class AppColor {
   static const mediumBlue = Color(0xFF4F59A2);
   static const primaryRed = Color(0xFFFD2424);
   static const primaryGreen = Color(0xFF31AF1A);
+  static const lightGreen = Color(0xFFE0ECDD);
+  static const primaryGrey = Color(0xFF656161);
+  static const mediumGreen = Color(0xFFC5EDBB);
 }
