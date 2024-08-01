74b8d4092895998ab1b7fe7000c7a67c9a006d00
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-08-01 16:19:24 -0300

    feat: i created the homeTabsProvider provider

--- lib/app/presentation/home/provider/home_provider.dart
+++ lib/app/presentation/home/provider/home_provider.dart
@@ -2,6 +2,12 @@ import 'package:control_concierge_agents/app/data/providers/data_provider.dart';
 import 'package:control_concierge_agents/app/presentation/home/states/agents_list_state_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../states/tabs_state_notifier.dart';
+
+final homeTabsProvider = StateNotifierProvider<TabsStateNotifier, int>(
+  (ref) => TabsStateNotifier(),
+);
+
 final agentListStateProvider =
     StateNotifierProvider<AgentListStateNotifier, AgentListState>(
   (ref) => AgentListStateNotifier(
