5e6ab5b50d651f0a58b48e93f9fdd1fba947102e
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-08-01 16:05:53 -0300

    feat: i created the UserDataSource class

---
+++ lib/app/data/data_source/user_data_source.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:control_concierge_agents/app/data/models/user_model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+import '../../core/helpers/errors/errors.dart';
+
+class UserDataSource {
+  final _firestore = FirebaseFirestore.instance;
+
+  Future<Either<CommonError, bool>> completeProfile({
+    required UserModel user,
+  }) async {
+    try {
+      final authUser = FirebaseAuth.instance.currentUser!;
+      user.photoUrl = await updateUserImage(user);
+
+      await _firestore.collection('users').doc(authUser.uid).set(user.toMap());
+
+      await authUser.updatePhotoURL(user.photoUrl);
+
+      await authUser.updateDisplayName(user.name);
+      return Right(true);
+    } on Exception catch (e) {
+      return Left(GenerateError.fromException(e));
+    }
+  }
+
+  Future<Either<CommonError, bool>> editUser({
+    required UserModel user,
+  }) async {
+    try {
+      final authUser = FirebaseAuth.instance.currentUser!;
+      user.photoUrl = await updateUserImage(user);
+
+      await _firestore.collection('users').doc(user.id).set(user.toMap());
+
+      await authUser.updatePhotoURL(user.photoUrl);
+
+      await authUser.updateDisplayName(user.name);
+      return Right(true);
+    } on Exception catch (e) {
+      return Left(GenerateError.fromException(e));
+    }
+  }
+
+  Future<Either<CommonError, UserModel>> getLocalUser() async {
+    try {
+      final authUser = FirebaseAuth.instance.currentUser!;
+
+      final getDocument =
+          await _firestore.collection('users').doc(authUser.uid).get();
+
+      final user = UserModel.fromSnapShot(getDocument);
+
+      return Right(user);
+    } on Exception catch (e) {
+      return Left(GenerateError.fromException(e));
+    }
+  }
+
+  Future<String?> updateUserImage(UserModel user) async {
+    String? image;
+    if (user.photoUrl != null && user.photoUrl!.isNotEmpty) {
+      if (!user.photoUrl!.contains(user.id)) {
+        final storageRef = FirebaseStorage.instance
+            .ref()
+            .child('users')
+            .child(user.id)
+            .child('uploads')
+            .child('${user.id}.png');
+        final uploadTask = storageRef.putFile(File(user.photoUrl!));
+        final TaskSnapshot downloadUrl = (await uploadTask);
+        final String imageUrl = await downloadUrl.ref.getDownloadURL();
+
+        image = imageUrl;
+      }
+    }
+    return image;
+  }
+}
