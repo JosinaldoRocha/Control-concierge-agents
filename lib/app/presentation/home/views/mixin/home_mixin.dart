9d9b6b64d532e079b33725f22a7b5e05bb37d3c4
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-08-01 16:21:31 -0300

    feat: i removed the logoutListen method and i called the ref.read method

--- lib/app/presentation/home/views/mixin/home_mixin.dart
+++ lib/app/presentation/home/views/mixin/home_mixin.dart
@@ -1,15 +1,11 @@
 import 'package:control_concierge_agents/app/presentation/authentication/provider/auth_provider.dart';
-import 'package:control_concierge_agents/app/presentation/authentication/states/logout/logout_state_notifier.dart';
-import 'package:control_concierge_agents/app/presentation/home/views/pages/home_page.dart';
+import 'package:control_concierge_agents/app/presentation/home/views/components/home_component.dart';
 import 'package:dropdown_textfield/dropdown_textfield.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../../core/style/app_colors.dart';
 import '../../../../data/enums/filter_type_enum.dart';
-import '../../../../widgets/snack_bar/app_snack_bar_widget.dart';
 import '../../provider/home_provider.dart';
 
-mixin HomeMixin<T extends HomePage> on ConsumerState<T> {
+mixin HomeMixin<T extends HomeComponent> on ConsumerState<T> {
   final bondTypeController = SingleValueDropDownController();
   final unitController = SingleValueDropDownController();
   final workShiftController = SingleValueDropDownController();
@@ -26,26 +22,7 @@ mixin HomeMixin<T extends HomePage> on ConsumerState<T> {
 
   void load() {
     ref.read(agentListStateProvider.notifier).load();
-  }
-
-  void logoutListen() {
-    ref.listen<LogoutState>(
-      logoutProvider,
-      (previous, next) {
-        next.maybeWhen(
-          loadSuccess: (data) =>
-              Navigator.of(context).pushReplacementNamed('/auth'),
-          loadFailure: (message) {
-            AppSnackBar.show(
-              context,
-              'Não foi possível sair do app. Tente novamente!',
-              AppColor.error,
-            );
-          },
-          orElse: () {},
-        );
-      },
-    );
+    ref.read(getUserProvider.notifier).load();
   }
 
   void onChanged(dynamic p0) {
