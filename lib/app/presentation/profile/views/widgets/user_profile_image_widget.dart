f5e5020989a33bad074bffd80cc43308f5333179
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-08-01 16:29:09 -0300

    feat: i created the UserProfileImageWidget widget

---
+++ lib/app/presentation/profile/views/widgets/user_profile_image_widget.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:loading_indicator/loading_indicator.dart';
+
+class UserProfileImageWidget extends StatelessWidget {
+  const UserProfileImageWidget({
+    super.key,
+    required this.image,
+    required this.size,
+  });
+
+  final String? image;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: image == null || image!.isEmpty
+          ? Center(
+              child: CircleAvatar(
+                radius: size,
+                backgroundImage: AssetImage('assets/images/profile.png'),
+              ),
+            )
+          : Container(
+              width: size,
+              height: size,
+              child: image!.contains('control-concierge-agents')
+                  ? ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: CachedNetworkImage(
+                        imageUrl: image!,
+                        fit: BoxFit.cover,
+                        placeholder: (context, url) => LoadingIndicator(
+                          indicatorType: Indicator.circleStrokeSpin,
+                        ),
+                      ),
+                    )
+                  : CircleAvatar(
+                      radius: size,
+                      backgroundImage: FileImage(File(image!)),
+                    ),
+            ),
+    );
+  }
+}
