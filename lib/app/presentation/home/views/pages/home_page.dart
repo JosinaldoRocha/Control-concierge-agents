f1aa5685dc10c0b30b9d476b6bbc2fdd2b34a912
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-08-01 16:23:06 -0300

    feat: I changed the entire scope by adding the bottom bar navigator and page view

--- lib/app/presentation/home/views/pages/home_page.dart
+++ lib/app/presentation/home/views/pages/home_page.dart
@@ -1,14 +1,12 @@
-import 'package:control_concierge_agents/app/presentation/home/provider/home_provider.dart';
-import 'package:control_concierge_agents/app/presentation/home/states/agents_list_state_notifier.dart';
-import 'package:control_concierge_agents/app/presentation/home/views/mixin/home_mixin.dart';
-import 'package:control_concierge_agents/app/presentation/home/views/widgets/filter_list_widget.dart';
-import 'package:control_concierge_agents/app/presentation/home/views/widgets/select_filter_option_widget.dart';
+import 'package:control_concierge_agents/app/core/style/app_colors.dart';
+import 'package:control_concierge_agents/app/core/style/app_text.dart';
+import 'package:control_concierge_agents/app/presentation/agent/views/component/add_edit_agent_component.dart';
+import 'package:control_concierge_agents/app/presentation/home/views/components/profile_component.dart';
+import 'package:control_concierge_agents/app/presentation/home/views/components/home_component.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:loading_indicator/loading_indicator.dart';
-import '../widgets/agent_list_widget.dart';
-import '../widgets/agent_search_delegate.dart';
-import '../widgets/drawer_widget.dart';
+
+import '../../provider/home_provider.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -17,127 +15,77 @@ class HomePage extends ConsumerStatefulWidget {
   ConsumerState<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends ConsumerState<HomePage> with HomeMixin {
+class _HomePageState extends ConsumerState<HomePage> {
+  int selectedIcon = 0;
+  PageController controller = PageController();
+
+  void listenTab() {
+    ref.listen<int>(
+      homeTabsProvider,
+      (previous, next) {
+        setState(() {
+          controller.jumpToPage(next);
+          selectedIcon = next;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final state = ref.watch(agentListStateProvider);
-    logoutListen();
+    listenTab();
 
     return Scaffold(
-      appBar: _buildAppBar(context, state),
-      body: Column(
-        children: [
-          FilterListWidget(
-            onTap: onTapFilter,
-          ),
-          if (filterType != null)
-            Container(
-              width: 300,
-              padding: const EdgeInsets.only(top: 16),
-              child: SelectFilterOptionWidget(
-                filterType: filterType,
-                bondTypeController: bondTypeController,
-                unitController: unitController,
-                workShiftController: workShiftController,
-                vacationPayController: vacationPayController,
-                onChanged: onChanged,
-              ),
-            ),
-          Expanded(
-            child: state.maybeWhen(
-              loadInProgress: () => _buildLoadingIndicator(),
-              loadSuccess: (data) {
-                return data.isEmpty
-                    ? const Center(
-                        child: Text('Nenhum agente cadastrado'),
-                      )
-                    : RefreshIndicator(
-                        onRefresh: () async => load(),
-                        child: AgentListWidget(
-                          agents: data,
-                          filterType: filterType,
-                          filter: filter,
-                        ),
-                      );
-              },
-              loadFailure: (failure) => const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Center(
-                  child: Text(
-                    'Houve um erro do nosso lado, tente novamente mais tarde!',
-                  ),
-                ),
-              ),
-              orElse: () => Container(),
-            ),
-          ),
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: const [
+          HomeComponent(),
+          AddEditAgentComponent(),
+          ProfileComponent(),
         ],
       ),
-      floatingActionButton: _buildFloatingActionButton(context),
-    );
-  }
-
-  Center _buildLoadingIndicator() {
-    return const Center(
-      child: SizedBox(
-        height: 10,
-        width: 80,
-        child: LoadingIndicator(
-          indicatorType: Indicator.ballPulse,
-        ),
-      ),
-    );
-  }
-
-  AppBar _buildAppBar(BuildContext context, AgentListState state) {
-    return AppBar(
-      title: const Text('Agentes de portaria SEMED'),
-      centerTitle: true,
-      leading: GestureDetector(
-        child: Icon(Icons.menu_rounded),
-        onTap: () => showModalBottomSheet(
-          isDismissible: false,
-          context: context,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10.0),
-              topRight: Radius.circular(10.0),
-            ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: _buildBottomBarItem(0, Icons.home_rounded),
+            label: 'Home',
           ),
-          builder: (context) => DrawerWidget(),
-        ),
-      ),
-      actions: [
-        IconButton(
-          icon: const Icon(
-            Icons.search_rounded,
-            size: 35,
+          BottomNavigationBarItem(
+            icon: _buildBottomBarItem(1, Icons.add_rounded),
+            label: 'Adicionar Agente',
           ),
-          onPressed: () {
-            showSearch(
-              context: context,
-              delegate: AgentSearchDelegate(
-                list: state.maybeWhen(
-                  loadSuccess: (data) => data,
-                  orElse: () => [],
-                ),
-              ),
-            );
-          },
-        ),
-      ],
+          BottomNavigationBarItem(
+            icon: _buildBottomBarItem(2, Icons.person_2_rounded),
+            label: 'Perfil',
+          ),
+        ],
+        elevation: 0,
+        currentIndex: selectedIcon,
+        backgroundColor: AppColor.white,
+        selectedLabelStyle: AppText.text().bodyMedium,
+        selectedIconTheme: IconThemeData(color: AppColor.bgColor),
+        onTap: (index) {
+          ref.read(homeTabsProvider.notifier).updateState = index;
+        },
+        iconSize: 26,
+      ),
     );
   }
 
-  FloatingActionButton _buildFloatingActionButton(BuildContext context) {
-    return FloatingActionButton(
-      child: const Icon(
-        Icons.add_rounded,
-        size: 40,
+  Container _buildBottomBarItem(
+    int index,
+    IconData icon,
+  ) {
+    return Container(
+      height: 44,
+      width: 44,
+      margin: EdgeInsets.only(top: 8),
+      decoration: BoxDecoration(
+        color: selectedIcon == index ? AppColor.primary : AppColor.lightGrey2,
+        shape: BoxShape.circle,
       ),
-      onPressed: () {
-        Navigator.pushNamed(context, '/agent/add');
-      },
+      child: Icon(icon),
     );
   }
 }
