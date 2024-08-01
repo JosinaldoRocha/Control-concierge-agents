08ad0a821381c2c3084e0e93379fa2c0819740e8
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-08-01 16:28:57 -0300

    feat: i created the SelectUserImageWidget widget

---
+++ lib/app/presentation/profile/views/widgets/select_user_image_widget.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../core/style/app_colors.dart';
+import '../../../../widgets/image/agent_profile_image.dart';
+
+class SelectUserImageWidget extends StatelessWidget {
+  const SelectUserImageWidget({
+    super.key,
+    required this.image,
+    required this.onTap,
+  });
+  final File? image;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: 104,
+        width: 104,
+        child: Stack(
+          children: [
+            AgentProfileImageWidget(
+              image: image?.path,
+              size: 100,
+            ),
+            Positioned(
+              top: 74,
+              left: 74,
+              child: InkWell(
+                onTap: onTap,
+                child: SvgPicture.asset(
+                  'assets/icons/edit.svg',
+                  colorFilter: const ColorFilter.mode(
+                    AppColor.secondary,
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
