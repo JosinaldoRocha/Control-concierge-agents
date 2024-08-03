dda12abe425a2ce75024b29e4a729ee08f7a2ee0
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-08-03 18:10:11 -0300

    feat: i created the VacationStatus enum

---
+++ lib/app/data/enums/vacation_status_enum.dart
@@ -0,0 +1,53 @@
+import 'package:control_concierge_agents/app/core/style/app_colors.dart';
+import 'package:flutter/material.dart';
+
+import '../models/vacation_model.dart';
+
+enum VacationStatus {
+  unexpired,
+  expired,
+  expiring,
+  undefined;
+
+  factory VacationStatus.status(VacationModel vacation) {
+    DateTime currentDate = DateTime.now();
+
+    if (vacation.vacationExpiration != null) {
+      if (vacation.vacationExpiration!.isAtSameMomentAs(currentDate)) {
+        return VacationStatus.expiring;
+      } else if (vacation.vacationExpiration!.isAfter(currentDate)) {
+        return VacationStatus.unexpired;
+      } else {
+        return VacationStatus.expired;
+      }
+    } else {
+      return VacationStatus.undefined;
+    }
+  }
+
+  String get text {
+    switch (this) {
+      case VacationStatus.unexpired:
+        return 'À vencer';
+      case VacationStatus.expired:
+        return 'Vencida';
+      case VacationStatus.expiring:
+        return 'Vencendo hoje';
+      case VacationStatus.undefined:
+        return 'À combinar';
+    }
+  }
+
+  Color get color {
+    switch (this) {
+      case VacationStatus.unexpired:
+        return AppColor.primaryGrey;
+      case VacationStatus.expiring:
+        return AppColor.primaryGreen;
+      case VacationStatus.expired:
+        return AppColor.primaryRed;
+      default:
+        return AppColor.primary;
+    }
+  }
+}
