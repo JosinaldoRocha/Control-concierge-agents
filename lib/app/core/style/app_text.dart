8679df2868856b32a237fb338a88c2173de64468
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-10-15 11:29:31 -0300

    feat: i changed the value of the titleLarge, titleMedium, bodyMedium and bodySmall parameters

--- lib/app/core/style/app_text.dart
+++ lib/app/core/style/app_text.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'app_colors.dart';
-
 class AppText {
   static TextTheme text() {
     return TextTheme(
-      titleLarge: GoogleFonts.inter(fontSize: 22),
-      titleMedium: GoogleFonts.inter(
+      titleLarge: GoogleFonts.dmSans(fontSize: 22),
+      titleMedium: GoogleFonts.dmSans(
+        fontWeight: FontWeight.w600,
         fontSize: 18,
-        color: AppColor.primary,
       ),
-      bodyMedium: GoogleFonts.poppins(),
-      bodySmall: GoogleFonts.inter(),
+      bodyMedium: GoogleFonts.dmSans(),
+      bodySmall: GoogleFonts.dmSans(fontSize: 12),
     );
   }
 }
