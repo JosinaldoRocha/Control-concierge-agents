180909cf6605900dca80182130d28bbe369b7ed8
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-10-15 11:38:47 -0300

    fix: i replaced the ElevatedButoon widget with the InkWell widget

--- lib/app/presentation/home/views/widgets/filter_item_widget.dart
+++ lib/app/presentation/home/views/widgets/filter_item_widget.dart
@@ -1,3 +1,4 @@
+import 'package:control_concierge_agents/app/core/style/app_text.dart';
 import 'package:control_concierge_agents/app/data/enums/filter_type_enum.dart';
 import 'package:flutter/material.dart';
 import '../../../../core/style/app_colors.dart';
@@ -16,16 +17,22 @@ class FilterItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        elevation: 0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(50),
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 12,
+          vertical: 4,
         ),
-        backgroundColor: isSelected ? AppColor.secondary : AppColor.primary,
+        child: Text(
+          filter.text,
+          style: AppText.text().bodyMedium?.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: isSelected ? AppColor.error : AppColor.black,
+              ),
+        ),
       ),
-      onPressed: onTap,
-      child: Text(filter.text),
     );
   }
 }
